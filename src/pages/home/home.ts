3d3c520f548eb449d7c918506b3c208a30c589c8
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-12-10 23:41:31 +0200

    u

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,9 +7,24 @@ import { NavController } from "ionic-angular";
   templateUrl: "home.html",
 })
 export class HomePage {
+  public mileage: number;
+  public oneLiterCost: number;
+  public liters: number;
+  public total: number;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  public initFields() {
+    if (this.oneLiterCost && this.liters) {
+      this.total = this.oneLiterCost * this.liters;
+    }
+    if (this.total && this.liters) {
+      this.oneLiterCost = this.total / this.liters;
+    }
+    if (this.total && this.oneLiterCost) {
+      this.liters = this.total / this.oneLiterCost;
+    }
+  }
 }

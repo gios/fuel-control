cd370c701a032aaee227c6fe7bea90c31b4c69ad
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-12-16 23:42:19 +0200

    u

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -19,30 +19,17 @@ export class MapPage implements AfterViewInit {
   }
 
   private loadMap() {
-    // make sure to create following structure in your view.html file
-    // <ion-content>
-    //  <div #map id="map"></div>
-    // </ion-content>
-
-    // create a new map by passing HTMLElement
     let element: HTMLElement = document.getElementById("map");
-
     let map = new GoogleMap(element);
 
-    // listen to MAP_READY event
     map.one(GoogleMapsEvent.MAP_READY).then(() => console.error("Map is ready!"));
-
-    // create LatLng object
     let ionic: GoogleMapsLatLng = new GoogleMapsLatLng(43.0741904, -89.3809802);
-
-    // create CameraPosition
     let position: CameraPosition = {
       target: ionic,
       tilt: 30,
       zoom: 18,
     };
 
-    // move the map's camera to position
     map.moveCamera(position);
   }
 }
